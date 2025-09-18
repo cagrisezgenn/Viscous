b3ee802b0440de87c1215148a7f05ded41f0cbc8
cagrisezgenn <hasmetcagri.sezgen@erbakan.edu.tr>
2025-09-19 01:25:58 +0300

    sil

--- GA_eski/set_pf_ton_if_nan.m
+++
@@ -1,8 +0,0 @@
-﻿function cfg2 = set_pf_ton_if_nan(cfg2, t5, dt_on)
-    if nargin<3 || isempty(dt_on), dt_on = 0.5; end
-    cfg2 = ensure_cfg_defaults(cfg2);
-    if ~isfield(cfg2,'PF') || ~isfield(cfg2.PF,'t_on') || isnan(cfg2.PF.t_on)
-        cfg2.PF.t_on = t5 + dt_on;
-    end
-end
-
